0b8ce95a5189bc8500d53072b1f1d94556ac0f58
Atharva Kolhe <agk7803@gmail.com>
2026-02-08 02:09:54 +0530

    secured api keys

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,22 +1,16 @@
-
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-
-// Import the functions you need from the SDKs you need
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDU1O5uPatpBXGg6FNpyEQTJVJ0cxte-LA",
-  authDomain: "stunotes-c9a11.firebaseapp.com",
-  projectId: "stunotes-c9a11",
-  storageBucket: "stunotes-c9a11.firebasestorage.app",
-  messagingSenderId: "155491108477",
-  appId: "1:155491108477:web:88be8d85ae3a773f159314"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
